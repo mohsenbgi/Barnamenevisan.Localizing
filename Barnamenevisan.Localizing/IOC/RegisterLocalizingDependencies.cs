54bf98dac71d45d90ad8fd09a7a06ad701f969d5
Mohsen bahgeri <mohsenbgi1380@gmail.com>
2023-07-16 11:49:03 +0330

    Add dbContext type to register dependencies

--- Barnamenevisan.Localizing/IOC/RegisterLocalizingDependencies.cs
+++ Barnamenevisan.Localizing/IOC/RegisterLocalizingDependencies.cs
@@ -1,14 +1,18 @@
 ﻿using Barnamenevisan.Localizing.Repository;
 using Barnamenevisan.Localizing.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Barnamenevisan.Localizing.IOC
 {
     public class RegisterLocalizingDependencies
     {
-        public static void Register(IServiceCollection services)
+        public static void Register(IServiceCollection services, Type DbContextType)
         {
-            services.AddScoped<ILocalizedPropertyRepository, LocalizedPropertyRepository>();
+            services.AddScoped<ILocalizedPropertyRepository, LocalizedPropertyRepository>(option =>
+            {
+                return new LocalizedPropertyRepository(option.GetRequiredService(DbContextType) as DbContext);
+            });
             services.AddScoped<ILocalizingService, LocalizinService>();
         }
     }
